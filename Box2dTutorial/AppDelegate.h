eee24404f7a37630b20a5adfd3a3c5fc660dabda
Ben Trengrove <b.trengrove@gmail.com>
2013-01-21 15:09:28 +1100

    Initial Commit

---
+++ Box2dTutorial/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Box2dTutorial
+//
+//  Created by Ben Trengrove on 21/01/13.
+//  Copyright Shiny Things 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Box2dTutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Box2dTutorial
+//
+//  Created by Ben Trengrove on 21/01/13.
+//  Copyright Shiny Things 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
